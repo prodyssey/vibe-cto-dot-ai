8fed27f978f746d52cd1b8a1e9e5e83133349bd7
Craig Sturgis <craig@craigsturgis.com>
2025-08-19 13:54:09 -0400

    fixed header layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         <Providers>
           <div id="root">{children}</div>
